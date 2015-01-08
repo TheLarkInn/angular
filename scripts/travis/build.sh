19342d1f5583ee100451ce8484a3ff835ed6ce79
Tobias Bosch <tbosch1009@gmail.com>
2015-01-08 14:20:05 -0800

    refactor(build): support multiple configurable browsers for e2e and perf tests
    Also includes browser configuration for Android

--- scripts/travis/build.sh
+++ scripts/travis/build.sh
@@ -4,30 +4,9 @@ set -e
 echo =============================================================================
 # go to project dir
 SCRIPT_DIR=$(dirname $0)
+source $SCRIPT_DIR/env_dart.sh
 cd $SCRIPT_DIR/../..
-source ./scripts/env.sh
 
 ./node_modules/.bin/gulp build
 
 pub install
-
-./node_modules/karma/bin/karma start karma-js.conf \
-        --reporters=dots \
-        --browsers=$BROWSERS --single-run
-./node_modules/karma/bin/karma start karma-dart.conf \
-        --reporters=dots \
-        --browsers=$BROWSERS --single-run
-
-./node_modules/.bin/webdriver-manager update
-
-function killServer () {
-  kill $serverPid
-}
-
-./node_modules/.bin/gulp serve.js.prod serve.js.dart2js&
-serverPid=$!
-
-trap killServer EXIT
-
-./node_modules/.bin/protractor protractor-e2e-js.conf.js
-./node_modules/.bin/protractor protractor-e2e-dart2js.conf.js
